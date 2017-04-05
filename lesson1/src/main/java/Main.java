3c1e9d455e482efa9ea6d3d465408eaa13b17b11
Pavel Skritskiy <pavel_working@mail.ru>
2017-04-05 23:32:59 +0300

    Обфускация

--- lesson1/src/main/java/Main.java
+++ lesson1/src/main/java/Main.java
@@ -1,10 +1,36 @@
 import au.com.bytecode.opencsv.CSVReader;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by pavel on 05.04.17.
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(CSVReader.class.getCanonicalName());
+    private static final int MAX_WINNERS_COUNT = 5;
+
+    public static void main(String[] args) throws IOException {
+        String pathToFile;
+        if (args.length >= 1) {
+            pathToFile = args[0];
+        } else {
+            pathToFile = "emails.csv";
+        }
+
+        System.out.println("Reading emails from: " + pathToFile);
+
+        CSVReader reader = new CSVReader(new FileReader(pathToFile));
+        List<String> emails = reader.readAll().stream().map(line -> line[0].trim()).collect(Collectors.toList());
+
+        System.out.println("Emails count: " + emails.size());
+
+        String seedString = "Some string";
+
+        List<String> winners = new LotteryMachine(emails, MAX_WINNERS_COUNT).setSeed(seedString).draw();
+
+        System.out.println("Winners:");
+        winners.forEach(System.out::println);
     }
 }
